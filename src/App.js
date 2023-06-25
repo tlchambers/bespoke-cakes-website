345af331754e40dd546e0b99ddbf0a70daf4333b
Thuy Ly-Chambers <thuylychambers@hotmail.com>
2023-06-25 22:28:14 +0100

    add cards and footer components

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import About from './components/About';
+import Cards from './components/Cards';
+import Footer from './components/Footer';
 
 function App() {
 	return (
@@ -9,6 +11,8 @@ function App() {
 			<Navbar />
 			<Hero />
 			<About />
+			<Cards />
+			<Footer />
 		</div>
 	);
 }

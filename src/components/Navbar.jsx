2c63422f5a6e00586daf29c5ec9cfdcdb54f421d
Thuy Ly-Chambers <thuylychambers@hotmail.com>
2023-06-26 12:56:18 +0100

    links now starts next to logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,93 +1,55 @@
-// import React, { useState } from 'react';
-// import { FaBars, FaTimes } from 'react-icons/fa';
-// import Logo from '../assets/Logo.png';
-
-// function Navbar() {
-// 	const [nav, setNav] = useState(false);
-// 	const handleClick = () => setNav(!nav);
-
-// 	return (
-// 		<div className="fixed flex justify-start items-center h-[150px] w-full mx-auto px-4 bg-[#f8f8f8]">
-// 			<div className="flex items-center">
-// 				<div>
-// 					<img src={Logo} alt="Logo" style={{ width: '220px' }} />
-// 				</div>
-// 				{/* Menu */}
-// 				<ul className="hidden ml-8 md:flex">
-// 					<li>Home</li>
-// 					<li>Bespoke Cakes</li>
-// 					<li>Price Guide</li>
-// 					<li>About</li>
-// 					<li>Contact</li>
-// 				</ul>
-// 			</div>
-
-// 			{/* Hamburger menu */}
-// 			<div onClick={handleClick} className="md:hidden z-10">
-// 				{!nav ? <FaBars /> : <FaTimes />}
-// 			</div>
-
-// 			{/* Mobile menu */}
-// 			<ul
-// 				className={
-// 					!nav
-// 						? 'hidden'
-// 						: 'absolute top-0 right-0 w-full h-screen  bg-[#f8f8f8] flex flex-col justify-center items-center'
-// 				}
-// 			>
-// 				<li className="py-6 text04xl">Home</li>
-// 				<li className="py-6 text04xl">Bespoke Cakes</li>
-// 				<li className="py-6 text04xl">Price Guide</li>
-// 				<li className="py-6 text04xl">About</li>
-// 				<li className="py-6 text04xl">Contact</li>
-// 			</ul>
-// 		</div>
-// 	);
-// }
-
-// export default Navbar;
-
 import React, { useState } from 'react';
 import { FaBars, FaTimes } from 'react-icons/fa';
 import Logo from '../assets/Logo.png';
 
 function Navbar() {
 	const [nav, setNav] = useState(false);
-	const handleClick = () => setNav(!nav);
+	const handleNav = () => {
+		setNav(!nav);
+	};
 
 	return (
-		<div className="fixed uppercase top-0 left-0 right-0 flex justify-start items-center h-[150px] w-full mx-auto px-2 bg-[#f8f8f8]">
-			<div className="relative z-20">
+		<div className="w-full flex items-center justify-between bg-[#f8f8f8]">
+			{/* Logo and Navigation Menu Container */}
+			<div className="flex items-center">
+				{/* Logo */}
 				<img src={Logo} alt="Logo" style={{ width: '220px' }} />
-			</div>
-			{/* Menu */}
-			<ul className="hidden px-4 md:flex">
-				<li>Home</li>
-				<li>Bespoke Cakes</li>
-				<li>Price Guide</li>
-				<li>About</li>
-				<li>Contact</li>
-			</ul>
 
-			{/* Hamburger menu */}
-			<div onClick={handleClick} className="md:hidden z-30">
-				{!nav ? <FaBars /> : <FaTimes />}
+				{/* Navigation Menu */}
+				<ul className="hidden sm:flex ml-8">
+					<li>Home</li>
+					<li>Bespoke Cakes</li>
+					<li>Price Guide</li>
+					<li>About</li>
+					<li>Contact</li>
+				</ul>
 			</div>
 
-			{/* Mobile menu */}
-			<ul
-				className={
-					!nav
-						? 'hidden'
-						: 'absolute top-[150px] right-0 w-full h-screen bg-[#f8f8f8] flex flex-col justify-center items-center'
-				}
-			>
-				<li className="py-6 text04xl">Home</li>
-				<li className="py-6 text04xl">Bespoke Cakes</li>
-				<li className="py-6 text04xl">Price Guide</li>
-				<li className="py-6 text04xl">About</li>
-				<li className="py-6 text04xl">Contact</li>
-			</ul>
+			{/* Mobile Menu */}
+			<div>
+				<ul
+					className={
+						!nav
+							? 'hidden'
+							: 'absolute top-[120px] right-0 w-full h-screen bg-[#f8f8f8] flex flex-col justify-center items-center'
+					}
+				>
+					<li className="py-8 text04xl ">Home</li>
+					<li className="py-8 text04xl">Bespoke Cakes</li>
+					<li className="py-8 text04xl">Price Guide</li>
+					<li className="py-8 text04xl">About</li>
+					<li className="py-8 text04xl">Contact</li>
+				</ul>
+			</div>
+
+			{/* Hamburger Icon */}
+			<div onClick={handleNav} className="sm:hidden pr-4">
+				{!nav ? (
+					<FaBars size={20} className="mr-4 cursor-pointer" />
+				) : (
+					<FaTimes size={20} className="mr-4 cursor-pointer" />
+				)}
+			</div>
 		</div>
 	);
 }

13e06ca811da13e9303ed23c7211bc28e1a79bad
Thuy Ly-Chambers <thuylychambers@hotmail.com>
2023-06-26 21:44:48 +0100

    cake page component added

---
+++ src/components/Cakes.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function Cakes() {
+	return (
+		<div name="cakes">
+			<h1>Cakes</h1>
+			<p>
+				Boujie Delights specialises in creating bespoke cakes that are tailored
+				to customers' specific needs. Offering a wide range of flavours,
+				fillings, and decorations ensures that your cake perfectly matches your
+				vision. Freshly baked buttercream cakes, cupcakes, and treats are not
+				only delicious, but also visually pleasing. Located in Brockley, South
+				East London.{' '}
+			</p>
+			<p>
+				Freshly baked buttercream cakes, cupcakes, and treats are not only
+				delicious, but also visually pleasing. Located in Brockley, South East
+				London.
+			</p>
+		</div>
+	);
+}
+
+export default Cakes;

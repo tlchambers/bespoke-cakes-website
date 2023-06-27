555e32fefc87ec57260f50b5ef5bfb05efb1125d
Thuy Ly-Chambers <thuylychambers@hotmail.com>
2023-06-27 16:16:22 +0100

    updated form layout and center it on page

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,17 +3,19 @@ import Img1 from '../assets/aboutImage.jpeg';
 
 function Contact() {
 	return (
-		<div name="contact" className="max-w-[1140px] m-auto w-full px-4 py-16">
-			<h2 className="text-4xl text-center uppercase">Contact Us</h2>
-			<p className="text-center py-2">Or say Hello!</p>
-			<div className="grid md:grid-cols-2">
-				<img
-					src={Img1}
-					alt="/"
-					className="w-full md:h-full object-cover p-2 max-h-[600px] h-[200]px"
-				/>
-				<form>
-					<div className="grid grid-cols-2">
+		<div
+			name="contact"
+			className="max-w-[1240px] m-auto w-full px-4 py-[4rem] flex flex-col items-center"
+		>
+			<h2 className="text-3xl py-6 uppercase text-center">Contact Us</h2>
+			<p className="text-center py-2">
+				If you are looking for a cake or treats for your special occasion,
+				Boujie Delights can help make your day remarkable.
+			</p>
+			<div className="grid md:grid-cols-2 py-4">
+				<img src={Img1} alt="/" className="w-full md:h-full object-cover" />
+				<form className="flex flex-col justify-center items-center">
+					<div className="grid grid-cols-2 w-full max-w-xl">
 						<input className="border m-2 p-2" type="text" placeholder="First" />
 						<input className="border m-2 p-2" type="text" placeholder="Last" />
 						<input
@@ -27,7 +29,9 @@ function Contact() {
 							rows={10}
 							placeholder="Message"
 						></textarea>
-						<button className="col-span-2 m-2 bg-pink-600">Submit</button>
+						<button className="uppercase col-span-2 m-2 bg-[#fee3c3] text-black hover:bg-white hover:border-2 hover:border-[#fee3c3]">
+							Submit
+						</button>
 					</div>
 				</form>
 			</div>

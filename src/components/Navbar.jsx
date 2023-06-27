782b5d4836f1b8037fdec6eb3df8e085236ed8c9
Thuy Ly-Chambers <thuylychambers@hotmail.com>
2023-06-27 15:26:23 +0100

    changed z-10 to z-20 to ensure navbar at the front

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
 	const handleClose = () => setNav(!nav);
 
 	return (
-		<div className="w-screen h-[80px] z-10 bg-zinc-200 fixed drop-shadow-lg">
+		<div className="w-screen h-[80px] z-20 bg-zinc-200 fixed drop-shadow-lg">
 			<div className="px-2 flex justify-between items-center w-full h-full">
 				<div className="flex items-center">
 					<h1 className="text-2xl mr-4 sm:text-4xl">Boujie Delights</h1>

868724da8e1c22a28ed3126fc306da7bb927bb72
Thuy Ly-Chambers <thuylychambers@hotmail.com>
2023-06-28 14:52:48 +0100

    update code to make alignment mobile friendly

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,17 +3,20 @@ import { Link } from 'react-scroll';
 
 function Hero() {
 	return (
-		<div name="home" className="w-full h-screen flex flex-col justify-between">
+		<div
+			name="home"
+			className="w-full h-screen flex flex-col items-center justify-center"
+		>
 			{/* background Image */}
 			<img
 				src="https://images.unsplash.com/photo-1595272568891-123402d0fb3b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MjB8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
 				alt=""
-				className="w-full h-full object-cover"
+				className="w-full h-full object-cover absolute inset-0 z-0"
 			/>
-			<div className="grid md:grid-cols-2 mx-w-[1240px] m-auto">
-				<div className="absolute top-[26%] flex flex-col ">
+			<div className="grid md:grid-cols-2 sm:grid-cols-1 mx-auto md:ml-0 md:pt-[12rem] lg:ml-[45rem] relative z-10">
+				<div className="flex flex-col justify-center items-center md:items-start sm:p-4 py-2">
 					<h3>Welcome To</h3>
-					<h2 className="md:text-4xl sm:text-4xl text-6xl md:py-4 py-2">
+					<h2 className="flex md:text-3xl sm:text-2xl text-4xl md:py-4 py-2">
 						Boujie Delights
 					</h2>
 					<p className="max-w-[400px] text-lg flex">

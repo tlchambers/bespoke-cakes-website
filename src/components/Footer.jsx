930823ba240a460b4a2923113ebb61d506c7b689
Thuy Ly-Chambers <thuylychambers@hotmail.com>
2023-07-18 22:02:01 +0100

    Updating business name

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,13 +7,13 @@ function Footer() {
 	return (
 		<div className="w-full mx-auto py-16 px-4 grid lg:grid-cols-3 gap-8 bg-[#f8f8f8]">
 			<div>
-				<h1 className="text-3xl">Boujie Delights</h1>
+				<h1 className="text-3xl">Beautifully Baked By Ess</h1>
 				<p className="py-4">
-					Step into the world of Boujie Delights, where every cake, cupcake, and
-					treat is lovingly handmade in Brockley, South East London. Our bespoke
-					creations are not only visually stunning, but also bursting with
-					flavor, ensuring that every bite is as unforgettable as the occasion
-					itself.
+					Step into the world of Beautifully Baked By Ess, where every cake,
+					cupcake, and treat is lovingly handmade in Brockley, South East
+					London. Our bespoke creations are not only visually stunning, but also
+					bursting with flavor, ensuring that every bite is as unforgettable as
+					the occasion itself.
 				</p>
 			</div>
 			<div className="lg:col-span-2 flex justify-between">
@@ -55,7 +55,7 @@ function Footer() {
 							<p className="flex align-items-start">
 								<GrLocation size={20} style={{ marginRight: '0.8rem' }} />
 								South-East London,
-								<br /> Forest Hill
+								<br /> Brockley
 							</p>
 						</li>
 						<li className="py-2 text-sm">
@@ -67,13 +67,13 @@ function Footer() {
 						<li className="py-2 text-sm">
 							<p className="flex items-center align-items-start">
 								<FaRegCommentDots size={20} style={{ marginRight: '0.8rem' }} />
-								boujiedelights@outlook.com
+								beautifullybakedbyess@outlook.com
 							</p>
 						</li>
 						<li className="py-2 text-sm">
 							<p className="flex items-center align-items-start">
 								<FaInstagram size={20} style={{ marginRight: '0.8rem' }} />
-								@boujiedelights
+								@beautifullybakedbyess
 							</p>
 						</li>
 					</ul>
